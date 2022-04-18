eda0ea48b3e3a89ff38164510c4fa97e3251904b
Mariia Bilinska <mariiaklochko@gmail.com>
2022-04-18 14:04:46 +0100

    Edit settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -67,7 +67,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-AUTH_USER_MODEL = "task_manager.User"
+AUTH_USER_MODEL = "task_manager.users.User"
 
 ROOT_URLCONF = 'task_manager.urls'
 

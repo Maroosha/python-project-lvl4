7bd9f4a93f9f39ccd1f069df6fd4875abb3e2380
Mariia Bilinska <mariiaklochko@gmail.com>
2022-04-29 13:43:04 +0100

    Add "login required" logic

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE_CLASSES = (
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.locale.LocaleMiddleware',  # translation
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -103,20 +103,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': os.environ.get("DB_NAME"),
-#        'USER': os.environ.get("DB_USER"),
-#        'PASSWORD': os.environ.get("DB_PASSWORD"),
-#        'HOST': os.environ.get("DB_HOST"),
-#        'PORT': os.environ.get("DB_PORT"),
-#    }
-#}
-
-#if not DEBUG:
-#    DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 database = dj_database_url.config(conn_max_age=500, ssl_require=True)
 
@@ -168,9 +154,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,5 +1,7 @@
-# from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
 from django.views.generic import CreateView, DeleteView, ListView, UpdateView
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy
@@ -41,34 +43,50 @@ class CreateUser(SuccessMessageMixin, CreateView):
         return context
 
 
-class ChangeUser(SuccessMessageMixin, UpdateView):
+class ChangeUser(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
     '.'
     model = User
     template_name = 'form.html'
     form_class = CreateUserForm
     success_url = reverse_lazy('users:list')
     success_message = gettext_lazy('User successfully changed.')
+    error_message = gettext_lazy('You do not have a permission to \
+change another user.')
 
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, request, **kwargs):
         '.'
         context = super().get_context_data(**kwargs)
         context['title'] = gettext_lazy('Change user')
         context['button_text'] = gettext_lazy('Change')
+        # forbid a user to change anyone else's account
+        if request.user != self.get_object():
+            messages.error(
+                self.request, self.error_message,
+            )
+            return redirect('users:list')
         return context
 
 
-class DeleteUser(SuccessMessageMixin, DeleteView):
+class DeleteUser(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
     '.'
     model = User
     template_name = 'delete.html'
     success_url = reverse_lazy('users:list')
     success_message = gettext_lazy('User successfully deleted.')
+    error_message = gettext_lazy('You do not have a permission to \
+change another user.')
 
 
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, request, **kwargs):
         '.'
         context = super().get_context_data(**kwargs)
         context['title'] = gettext_lazy('Delete user')
         context['button_text'] = gettext_lazy('Delete')
+        # forbid a user deleting anyone else's account
+        if request.user != self.get_object():
+            messages.error(
+                self.request, self.error_message,
+            )
+            return redirect('users:list')
         return context

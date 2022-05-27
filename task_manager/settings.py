09dc04e5eb683d70e380bf94538b745c3eef4c89
Mariia Bilinska <mariiaklochko@gmail.com>
2022-05-27 13:50:06 +0100

    Fixing Войти -> Вход

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -138,10 +138,10 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = os.getenv('LOCALE', default='ru')
 
 TIME_ZONE = 'UTC'
-#TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 

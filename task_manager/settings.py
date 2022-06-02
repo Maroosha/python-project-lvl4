644f5b6f2ddf6f1c7b43b9a61b3ebc9c4eef79e8
Mariia Bilinska <mariiaklochko@gmail.com>
2022-06-02 11:29:12 +0100

    Change remote app name

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY'),
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "afternoon-shore-73997.herokuapp.com",
+    "enigmatic-spire-21419.herokuapp.com",
     "localhost",
     "127.0.0.1",
     "webserver",
